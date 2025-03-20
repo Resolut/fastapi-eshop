2bd9d831f45cbfc469ce2c3c5436d4f699e82055
Dmitry Samofalov <samdimus@gmail.com>
2025-03-20 17:59:51 +0300

    implement permissions functions for deleting and changing user roles

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.routers.categories import router as categories_router
 from app.routers.products import router as products_router
 from app.routers.auth import router as auth_router
-
+from app.routers.permission import router as permission_router
 
 app = FastAPI()
 
@@ -14,3 +14,4 @@ async def welcome():
 app.include_router(categories_router)
 app.include_router(products_router)
 app.include_router(auth_router)
+app.include_router(permission_router)
